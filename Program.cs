5cab8a0ec0246f10d7aaf5f666455b1200812de8
Lugh-o <lughfalcao@gmail.com>
2024-08-05 12:02:45 -0300

    Implementação do desafio

--- Program.cs
+++ Program.cs
@@ -1,3 +1,11 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Iphone iphone1 = new Iphone("1", "pro max", "11223344", 16);
+iphone1.Ligar();
+iphone1.ReceberLigacao();
+iphone1.InstalarAplicativo("Instagram");
+
+Nokia nokia1 = new Nokia("1", "nokia", "11223344", 16);
+nokia1.Ligar();
+nokia1.ReceberLigacao();
+nokia1.InstalarAplicativo("Instagram");
